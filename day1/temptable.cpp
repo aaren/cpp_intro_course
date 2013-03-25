f9b35e4f1fd18027cf248a1d2a0769f551ed5a5e
Aaron O'Leary <eeaol@leeds.ac.uk>
2013-03-25 16:14:08 +0000

    refactor to functions

--- day1/temptable.cpp
+++ day1/temptable.cpp
@@ -21,10 +21,31 @@ AOL, C++ Intro course, March 25 2012
 
 using namespace std;
 
-double conv_F_to_C (double T_F) {
+double celsiusTemperature (double T_F) {
   // Convert fahrenheit to celsius
   double T_C = 5 * (T_F - 32) / 9;
   return T_C;
+}
+
+int printTable(vector<double> T_F, vector<double> T_C) {
+  // Print a table of two input vectors, fahrenheit and
+  // celsius
+
+  // set column widths
+  int c1 = 14;
+  int c2 = 12;
+  cout << setw(c1 + c2) << "Temperature conversion table" << endl;
+  cout << setw(c1) << "Fahrenheit";
+  cout << " | "; 
+  cout << setw(c2) << "Celsius" << endl;
+  // set precision
+  int p = 3;
+  for (int i = 0; i < T_F.size(); i++){
+    cout << setw(c1) << fixed << setprecision(p) << T_F[i];
+    cout << " | ";
+    cout << setw(c2) << fixed << setprecision(p) << T_C[i] << endl;
+  }
+  return 0;
 }
 
 int main() {
@@ -56,24 +77,10 @@ int main() {
   // Create vector of ouput temperatures
   vector<double> output_T;
   for (int i = 0; i < input_T.size(); i++){
-    output_T.push_back(conv_F_to_C(input_T[i]));
+    output_T.push_back(celsiusTemperature(input_T[i]));
   }
-
-  // Print a table
-  // column widths
-  int c1 = 14;
-  int c2 = 12;
-  cout << setw(c1 + c2) << "Temperature conversion table" << endl;
-  cout << setw(c1) << "Fahrenheit";
-  cout << " | "; 
-  cout << setw(c2) << "Celsius" << endl;
-  // set precision
-  int p = 3;
-  for (int i = 0; i < input_T.size(); i++){
-    cout << setw(c1) << fixed << setprecision(p) << input_T[i];
-    cout << " | ";
-    cout << setw(c2) << fixed << setprecision(p) << output_T[i] << endl;
-  }
+  
+  printTable(input_T, output_T);
 
   return 0; 
 }
