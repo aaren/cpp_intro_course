c6cf5fedf513024a023ef30ec30e9435a07daab3
Aaron O'Leary <eeaol@leeds.ac.uk>
2013-03-26 10:54:50 +0000

    calculate median

--- day2/stats.cpp
+++ day2/stats.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <fstream>
 #include <numeric>
+#include <algorithm>
 
 using namespace std;
 
@@ -24,12 +25,38 @@ void readData (istream& input, vector<double>& data)
 double mean (const vector<double>& data)
 {
   // n.b. reference
+  // TODO: why is it reference here?? is this necessary?
   // Compute mean of the values stored in the vector
   // N.B. third arg in accumulate is init and has to be the
   // *same* type as the stuff in the vector (double here)
   double total = accumulate(data.begin(), data.end(), 0.0);
   double mean = total / data.size();
   return mean;
+}
+
+double median (const vector<double>& data)
+{
+  // compute the median of an input vector
+  // sort the vector
+  // why does this give errors when doing it outside doesn't??
+  // sort(data.begin(), data.end());
+  // length of the vector
+  int len = data.size();
+  double med;
+  if (len % 2 == 0) {
+    // even length
+    med = (data[len / 2 - 1] + data[len / 2]) / 2;
+  }
+  else if (len % 2 == 1) {
+    // odd length
+    med = data[len / 2];
+  }
+  else {
+    // erm...
+    cout << "Weird vector!";
+    return 1;
+  }
+  return med;
 }
 
 int main()
@@ -50,6 +77,10 @@ int main()
 
   // compute and print the mean
   cout << "Mean value = " << mean(data) << endl;
+
+  // compute and print the median
+  sort(data.begin(), data.end());
+  cout << "Median value = " << median(data) << endl;
 
   return 0;
 }
